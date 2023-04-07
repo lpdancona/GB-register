9a55fd3de269b17d7126678811c8044e54d7d53e
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-04-06 17:29:24 -0700

    implemented style for navbar, login and register

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import "./navbar.scss";
+import Logo from "../../docs/gb-logo.svg";
+
 export default function Navbar() {
+  const username = JSON.parse(localStorage.getItem("username"));
+  const logout = function () {
+    window.localStorage.clear();
+    window.location.reload();
+  };
   return (
     <div>
-      <nav class="fixed-nav-bar">
-        <a class="brand"></a>
-        <a class="link" href="#">
-          archive
-        </a>
-        <a class="link" href="#">
-          projects
-        </a>
+      <nav className="fixed-nav-bar">
+        <img src={Logo} alt="" className="nav-logo" />
+        <div className="dropdown-buttons">
+          <div className="dropdown">
+            <button className="dropbtn">Options</button>
+            <div className="dropdown-content">
+              <a href="#">Classes</a>
+              <a href="#">Check-Ins</a>
+              <a href="#">Create</a>
+            </div>
+          </div>
+          {username ? (
+            <div>
+              <button onClick={logout} className="new-movie-button">
+                Log-out
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button className="new-movie-button">Log-in</button>
+              <button className="new-movie-button">Register</button>
+            </div>
+          )}
+        </div>
       </nav>
     </div>
   );
